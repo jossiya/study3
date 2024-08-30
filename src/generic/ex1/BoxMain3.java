8e04546967b9c313c7003531f969f0d23c0ee3e3
AIM <rolety2202@naver.com>
2024-08-30 11:42:15 +0900

    Generic

---
+++ src/generic/ex1/BoxMain3.java
@@ -0,0 +1,24 @@
+package generic.ex1;
+
+public class BoxMain3 {
+    public static void main(String[] args) {
+        GenericBox<Integer> integerBox = new GenericBox<Integer>(); //생성 시점에 T의 타입 결정
+        integerBox.set(10);
+        //integerBox.set("문자100"); // Integer 타입만 허용, 컴파일 오류
+        Integer integer = integerBox.get();
+        System.out.println("integer = " + integer); // Integer 타입 변환(캐스팅 X)
+
+        GenericBox<String> stringBox = new GenericBox<String>();
+        stringBox.set("hello");
+        String str = stringBox.get();
+        System.out.println("str = " + str);
+
+        GenericBox<Double> doubleBox = new GenericBox<Double>();
+        doubleBox.set(10.5);
+        Double doubleValue = doubleBox.get();
+        System.out.println("doubleValue = " + doubleValue);
+
+        //타입 추론: 생성하는 제네릭 타입 생략 가능
+        GenericBox<Integer> integerBox2 = new GenericBox<>();
+    }
+}
